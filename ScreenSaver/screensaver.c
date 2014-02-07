41b85833cc1074f51fbf50a2de710b73232efd23
U-Mike-PC\Mike <Mike@Mike-PC.(none)>
2014-02-06 22:22:03 -0400

    Morphing other half of N

--- ScreenSaver/screensaver.c
+++ ScreenSaver/screensaver.c
@@ -18,6 +18,7 @@
 
 // Interporlation variable
 float interp = 0.0;
+int interpFlip = 0;
 
 typedef float x_y_coord[2];
 
@@ -85,7 +86,7 @@ void drawStar(void) {
 
 	// clear the screen
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+
 	// First half of Star
 	glBegin(GL_TRIANGLE_FAN);
 		// Set color to RED
@@ -115,35 +116,33 @@ void drawLetter(void) {
 
 	// clear the screen
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+
 	// First half of N
 	glBegin(GL_TRIANGLE_FAN);
 		// Set color to RED
 		glColor3f(1, 0, 0);
 		// First part of triangle fan
 		for(i=0; i<7; i++) {
-			switch(i) 
+			switch(i)
 			{
+				case 0:
+					glVertex2f((1-interp) * left_half_n[1][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[1][1] + interp * left_half_star[i][1]);
+					break;
 				case 1:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[2][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[2][1]);
+					glVertex2f((1-interp) * left_half_n[0][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[0][1] + interp * left_half_star[i][1]);
 					break;
 				case 2:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[1][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[1][1]);
+					glVertex2f((1-interp) * left_half_n[6][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[6][1] + interp * left_half_star[i][1]);
+					glVertex2f((1-interp) * left_half_n[5][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[5][1] + interp * left_half_star[i][1]);
 					break;
 				case 3:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[6][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[6][1]);
+					glVertex2f((1-interp) * left_half_n[4][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[4][1] + interp * left_half_star[i][1]);
 					break;
 				case 4:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[5][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[5][1]);
+					glVertex2f((1-interp) * left_half_n[3][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[3][1] + interp * left_half_star[i][1]);
 					break;
 				case 5:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[4][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[4][1]);
-					break;
-				case 6:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[3][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[3][1]);
-					break;
-				case 7:
-					glVertex2f((1-interp) * left_half_n[i][0] + interp * left_half_star[3][0], (1-interp) * left_half_n[i][1] + interp * left_half_star[3][1]);
+					glVertex2f((1-interp) * left_half_n[2][0] + interp * left_half_star[i][0], (1-interp) * left_half_n[2][1] + interp * left_half_star[i][1]);
 					break;
 				default :
 					break;
@@ -151,21 +150,38 @@ void drawLetter(void) {
 		}
 	glEnd();
 
-	/*
 	// Second half of N
 	glBegin(GL_TRIANGLE_FAN);
 		// Set color to RED
 		glColor3f(1, 0, 0);
 		// First part of triangle fan
 		for(i=0; i<7; i++) {
-			if(i == 7) {
-				glVertex2f((1-interp) * right_half_n[i][0] + interp * right_half_star[6][0], (1-interp) * right_half_n[i][1] + interp * right_half_star[6][1]);
-			} else {
-				glVertex2f((1-interp) * right_half_n[i][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[i][1] + interp * right_half_star[i][1]);
+		switch(i)
+			{
+				case 0:
+					glVertex2f((1-interp) * right_half_n[5][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[5][1] + interp * right_half_star[i][1]);
+					break;
+				case 1:
+					glVertex2f((1-interp) * right_half_n[6][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[6][1] + interp * right_half_star[i][1]);
+					break;
+				case 2:
+					glVertex2f((1-interp) * right_half_n[0][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[0][1] + interp * right_half_star[i][1]);
+					break;
+				case 3:
+					glVertex2f((1-interp) * right_half_n[1][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[1][1] + interp * right_half_star[i][1]);
+					break;
+				case 4:
+					glVertex2f((1-interp) * right_half_n[2][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[2][1] + interp * right_half_star[i][1]);
+					break;
+				case 5:
+					glVertex2f((1-interp) * right_half_n[3][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[3][1] + interp * right_half_star[i][1]);
+					glVertex2f((1-interp) * right_half_n[4][0] + interp * right_half_star[i][0], (1-interp) * right_half_n[4][1] + interp * right_half_star[i][1]);
+					break;
+				default :
+					break;
 			}
 		}
 	glEnd();
-	*/
 
 }
 
@@ -173,12 +189,18 @@ void drawLetter(void) {
 void myIdle(void)
 {
 
-	// update the interpolation variable 
-	interp += 0.00005;
+	// update the interpolation variable depending on value of the flip variable
+	if (interpFlip == 0) {
+		interp += 0.00005;
+	} else if (interpFlip == 1) {
+		interp -= 0.00005;
+	}
 
-
+	// Flips the interp to subtract or add, depending on the part of the morph it is in
 	if (interp >= 1.0) {
-		interp = 0.0;
+		interpFlip = 1;
+	} else if (interp <= 0.0 && interpFlip != 0) {
+		interpFlip = 0;
 	}
 
 	// now force OpenGL to redraw the change
@@ -197,7 +219,6 @@ void display(void)
 {
 
 	drawLetter();
-	//drawStar();
 
 	// send all output to display
 	glFlush();
@@ -227,7 +248,7 @@ void main(int argc, char** argv)
 	// register redraw function
 	glutDisplayFunc(display);
 	// register the idle function
-	glutIdleFunc(myIdle);     
+	glutIdleFunc(myIdle);
 	//initialize the rendering context
 	init();
 	// go into a perpetual loop
